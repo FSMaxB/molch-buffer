1e750a3f18596aac514e38813e1bcbe2ea9a7ecf
Max Bruckner <max@maxbruckner.de>
2015-10-01 13:16:27 +0200

    FIX: incorrect content length when copyiing a buffer
    If the content_length is already longer than what's copied into the
    buffer, the length was reduced by buffer_copy.

--- buffer.c
+++ buffer.c
@@ -188,7 +188,6 @@ int buffer_copy(
 	destination->content_length = (destination->content_length > destination_offset + copy_length)
 		? destination->content_length
 		: destination_offset + copy_length;
-	destination->content_length = destination_offset + copy_length;
 
 	return 0;
 }
